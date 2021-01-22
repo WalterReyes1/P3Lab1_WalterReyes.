cd5cede980afa22487da0ae5346a8a75df1dac93
walter reyes <walter.reyes@cambridgehn.com>
2021-01-22 15:15:04 -0600

    Final

--- main.cpp
+++ main.cpp
@@ -3,29 +3,40 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 int main() {
-	cout<<"1. Sumatoria \n";
-	cout<<"2.Formula \n";
-	cout<<"3.Salir \n";
 	int menu;
 	menu = 0;
-	cin>> menu;
 	while(menu != 3){
+		cout<<"1. Sumatoria \n";
+		cout<<"2.Formula \n";
+		cout<<"3.Salir \n";
+		cin>> menu;
 
 	if(menu ==1){
 		float k,aux,total;
 		cout<<"Sumatoria \n";
 		cout<<"Ingrese un valor para k: ";
 		cin>>k;
+		while(k<0){
+			cout<<"Ingrese un valor para k que sea positivo: ";
+			cin>>k;	
+			}
 		for(int i = 0; i<=k; i++){
-		aux= i*pow(i+1,2);
-		total= total+aux;
+			aux= i*pow(i+1,2);
+			total= total+aux;
 		}
 		cout<< "El total es: ";
 		cout<<total;
 		
 	menu=3;
 	} if(menu==2){
-			cout<<"2.mundo";
+			float x = 0;
+			float total_1 = 0;
+			cout<<"Formula \n";
+			cout<<"Ingrese un valor para x: ";
+			cin>>x;
+			total_1 = 1/(1 +exp(x));
+			cout<<"La respuesta es: ";
+			cout<< total_1;
 	menu=3;
 	}
 		
